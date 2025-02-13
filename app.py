a7e390593e1cbccfcf4ca7179e51ed471f0b7e99
Temp Converter <temp-converter@example.com>
2025-02-13 01:11:36 -0500

    Initial commit - Temperature converter app

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    data = request.get_json()
+    temp = float(data['temp'])
+    unit = data['unit']
+    
+    if unit == 'C':
+        result = (temp * 9/5) + 32
+        new_unit = 'F'
+    else:
+        result = (temp - 32) * 5/9
+        new_unit = 'C'
+        
+    return jsonify({
+        'result': round(result, 2),
+        'unit': new_unit
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
